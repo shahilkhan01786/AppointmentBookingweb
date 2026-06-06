ea7c104da3b050a94e77a839d329ad5a3fb09aad
shahilkhan01786 <mdshahil742445@gmail.com>
2026-06-06 16:17:52 +0530

    Update saveAppointment function to use new API URL

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function saveAppointment() {
 
     try {
 
-        const response = await fetch("http://127.0.0.1:5000/book", {
+        const response = await fetch("https://appointment-booking01.onrender.com", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
